4df31dc22821cdc139f579befa05530b651f8322
peckpeck20 <peckpeckthebeast@gmail.com>
2018-06-24 22:26:40 +0300

    v.1.0 out

--- App.js
+++ App.js
@@ -1,63 +1,29 @@
 import React from 'react';
-// import { StyleSheet, Text, View, TextInput } from 'react-native';
-import { Container, Header, Content, Footer, Text,Input, Icon,Item,Button } from 'native-base'; 
+import MainScreen from './components/Main';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.clearAllFields = this.clearAllFields.bind(this);
+
     this.state = {
-      optionOne : '',
-      optionTwo : ''
-    }
+      isReady: false
+    };
   }
 
-  randomize(){
-    const randomNumber = Math.floor((Math.random() * 10) + 1);
-    console.log(randomNumber);
-    
+  componentWillMount() {
+    this.loadFonts();
   }
-
-  clearAllFields(){
-    this.setState({
-      optionOne : '',
-      optionTwo : ''
-    })
+  async loadFonts() {
+    await Expo
+      .Font
+      .loadAsync({Roboto: require("native-base/Fonts/Roboto.ttf"), Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"), Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")});
+    this.setState({isReady: true});
   }
-
-  async componentWillMount() { await Expo.Font.loadAsync({ 'Roboto': require('native-base/Fonts/Roboto.ttf'), 'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'), }); }
 
   render() {
-    return (
-    <Container > 
-      <Header /> 
-        <Content padder> 
-          <Item>
-              <Icon active name='home' />
-              <Input placeholder='Option 1'
-              onChangeText={optionOne => this.setState({ optionOne })} value={this.state.optionOne}/>
-          </Item>
-          <Item>
-              <Icon active name='home' />
-              <Input placeholder='Option 2'
-              onChangeText={optionTwo => this.setState({ optionTwo })} value={this.state.optionTwo}/>
-          </Item>
-          <Item>
-            <Button iconLeft onPress={this.randomize}>
-              <Icon name='home' />
-              <Text>GO!</Text>
-            </Button>
-            <Button iconLeft dark onPress={this.clearAllFields}>
-              <Icon name='cog' />
-              <Text>clear</Text>
-          </Button>
-          </Item>
-          <Text>{this.state.optionOne}</Text>
-        </Content> 
-       <Footer /> 
-      </Container>
-      )
+    if (!this.state.isReady) {
+      return <Expo.AppLoading/>;
+    }
+    return (<MainScreen/>)
   }
 }
-
-
---
+++ components/Main.js
@@ -0,0 +1,142 @@
+import React, {Component} from 'react';
+import {
+    Container,
+    Header,
+    Content,
+    Footer,
+    Text,
+    Input,
+    Icon,
+    Item,
+    Button,
+    H1
+} from 'native-base';
+import {Col, Row, Grid} from 'react-native-easy-grid';
+
+export default class MainScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.clearAllFields = this
+            .clearAllFields
+            .bind(this);
+        this.randomize = this
+            .randomize
+            .bind(this);
+        this.state = {
+            optionOne: 'Apple',
+            optionTwo: 'Bananas',
+            isRemainder: ''
+        }
+    }
+
+    randomize() {
+        const randomNumber = Math.floor((Math.random() * 10) + 1);
+        //check if number has a Remainder after division
+        const isRemainder = randomNumber % 2;
+        this.setState({isRemainder});
+
+        //input field validation
+        if (this.state.optionOne && this.state.optionTwo) {
+
+            if (isRemainder === 1) {
+                alert(`Go for ${this.state.optionOne}`)
+            } else {
+                alert(`Go for ${this.state.optionTwo}`)
+            }
+            // this.clearAllFields();
+        } else {
+            alert('Field empty, Try again')
+        }
+
+    }
+
+    clearAllFields() {
+        this.setState({optionOne: '', optionTwo: '', randomNumber: 0})
+    }
+
+    render() {
+        return (
+            <Container >
+                <Header/>
+                <Content padder>
+                    <Grid>
+                        <Row
+                            size={2}
+                            style={{
+                            padding: 20
+                        }}>
+                            <Col>
+                                <Item>
+                                    <Icon
+                                        active
+                                        name='thumbs-up'
+                                        style={{
+                                        color: 'green'
+                                    }}/>
+                                    <Input
+                                        placeholder='Option 1'
+                                        onChangeText={optionOne => this.setState({optionOne})}
+                                        value={this.state.optionOne}/>
+                                </Item>
+                            </Col>
+
+                        </Row>
+                        <Row
+                            size={1}
+                            style={{
+                            padding: 20
+                        }}>
+                            <Col size={1}></Col>
+                            <Col size={1}>
+                                <H1>Versus</H1>
+                            </Col>
+                            <Col size={1}></Col>
+
+                        </Row>
+                        <Row
+                            size={1}
+                            style={{
+                            padding: 20
+                        }}>
+                            <Col>
+                                <Item>
+                                    <Icon
+                                        active
+                                        name='thumbs-down'
+                                        style={{
+                                        color: 'red'
+                                    }}/>
+                                    <Input
+                                        placeholder='Option 2'
+                                        onChangeText={optionTwo => this.setState({optionTwo})}
+                                        value={this.state.optionTwo}/>
+                                </Item>
+                            </Col>
+                        </Row>
+                        <Row
+                            size={2}
+                            style={{
+                            padding: 20
+                        }}>
+                            <Col>
+                                <Button iconLeft info onPress={this.clearAllFields}>
+                                    <Icon name='format-clear' type="MaterialCommunityIcons"/>
+                                    <Text>clear</Text>
+                                </Button>
+                            </Col>
+                            <Col></Col>
+                            <Col>
+                                <Button success iconLeft onPress={this.randomize}>
+                                    <Icon name='check' type="MaterialCommunityIcons"/>
+                                    <Text>Go!!</Text>
+                                </Button>
+                            </Col>
+
+                        </Row>
+                    </Grid>
+                </Content>
+                <Footer/>
+            </Container>
+        )
+    }
+};

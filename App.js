2f991e207ae5fe1aab698147baf202b66d513f92
Jose Zapata <jose.zapata@columbiaroad.com>
2020-02-05 21:28:20 +0200

    sdk -> 36 upgrade

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppLoading } from 'expo';
+import { AppLoading, Font } from 'expo';
 import { MainScreen } from './components/Main';
 
 export default class App extends React.Component {
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
-    await Expo.Font.loadAsync({
+    await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       //Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
